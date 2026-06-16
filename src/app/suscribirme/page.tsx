8b10471c35910712800d0c3039940311ac1497b9
karenlazz <karen.eklo@gmail.com>
2026-06-15 22:22:48 -0600

    Update page.tsx

--- src/app/suscribirme/page.tsx
+++ src/app/suscribirme/page.tsx
@@ -2,6 +2,8 @@ import { SectionHeading } from "@/components/section-heading";
 import { SubscribeForm } from "@/components/subscribe-form";
 import { listMuseums } from "@/lib/data";
 
+export const dynamic = "force-dynamic";
+
 export default async function SubscribePage() {
   const museums = await listMuseums();
   const boroughs = Array.from(new Set(museums.map((museum) => museum.borough).filter(Boolean))) as string[];
