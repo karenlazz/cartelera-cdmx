0045c3fb4ff2ad411a751ec5c2b50685e4333516
karenlazz <karen.eklo@gmail.com>
2026-06-15 22:19:29 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import { SectionHeading } from "@/components/section-heading";
 import { listMuseums, listPublicExhibitions } from "@/lib/data";
 import type { ExhibitionFilters } from "@/lib/types";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home({
   searchParams
 }: {
